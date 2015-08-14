20f2f96e0645520b089b36b6cec24e3c3f0469e4
Ahmed El-Taweel <ahmedeltaweel@users.noreply.github.com>
2015-08-14 10:11:43 +0200

    Create 518A - Vitaly and Strings .cpp

---
+++ 518A - Vitaly and Strings .cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string a;
+string b;
+string c;
+int main() {
+	cin >> a >> b;
+	c = a;
+	for (int i = a.size() - 1; i >= 0; i--) {
+		if (c[i] == 'z')
+			c[i] -= 25;
+		else {
+			c[i]++;
+			break;
+		}
+	}
+	int flag = 0;
+	for (int i = 0; i < a.size(); i++) {
+		if (c[i] < b[i]) {
+			flag = 1;
+			break;
+		}
+		if (c[i] > b[i]) {
+			flag = 0;
+			break;
+		}
+	}
+	if (flag == 0)
+		cout << "No such string" << endl;
+	else
+		cout << c << endl;
+
+	return 0;
+}
